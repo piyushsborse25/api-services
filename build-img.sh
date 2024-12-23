0943451bc3a526860353aaffdab9b4ac87940bab
piyushsborse25 <piyushsborse25@gmail.com>
2024-12-24 00:42:53 +0530

    Modified build script

--- build-img.sh
+++ build-img.sh
@@ -4,4 +4,6 @@ cd /home/piyush/Documents/projects/Container
 
 docker build -t piyushsborse/automated-service:latest -f Dockerfile .
 
+#docker stop piyushsborse/automated-service:latest
+
 docker run -d -p 8080:8080 piyushsborse/automated-service:latest
